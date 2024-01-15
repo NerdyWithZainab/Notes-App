35f80461c3da746229f842509e42f3d9f27b8bd4
Zainab Rangwala <zfr2000@hw.ac.uk>
2024-01-15 13:09:10 +0400

    Edited the file to test the code

--- notes/test/widget_test.dart
+++ notes/test/widget_test.dart
@@ -7,12 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:notes/views/register_view.dart';
+
+import 'package:notes/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const RegisterView());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
